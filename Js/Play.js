b2f9bcdbbe616509e540bd8ff2328ad0cf5415d3
Damien <damien.kemp@students.cau.edu>
2021-09-07 00:10:30 -0400

    added javascript to the file

---
+++ Js/Play.js
@@ -0,0 +1,17 @@
+function playingSound (e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key ="${e.keyCode}"]`)
+    if(!audio) return //stops the function from running
+    audio.currentTime = 0; //rewinds it to the start
+    audio.play();
+    key.classList.add('playing');
+  }
+
+  function removeTransition(e) {
+    if(e.propertyName !== 'transform') return // skips if it's not a transform
+    this.classList.remove('playing') //removes transform
+  }
+  
+  const keys = document.querySelectorAll('.key')
+  keys.forEach(key => key.addEventListener('transitionend',removeTransition))
+  window.addEventListener('keydown',playingSound);
